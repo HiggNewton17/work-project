73868fd099ef7aaace5587d4aebda158e6a81e22
HiggNewton17 <tj10packers@yahoo.com>
2018-03-14 09:48:36 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const gageFactor = document.getElementById("gageFactor").value;
+const bridgeResistance = document.getElementById("bridgeResistance").value;
+const strain = document.getElementById("strain").value;
+const tempCoeffRes = document.getElementById("tempCoeffRes").value;
+const deltaT = document.getElementById("deltaT").value;
+
+//if microstrain <= 85 don't perform balance
