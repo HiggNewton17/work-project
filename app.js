0e335b484290937ccf82b0cf0164a6dc0278073e
HiggNewton17 <tj10packers@yahoo.com>
2018-03-14 14:01:51 -0400

    working on functionality

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const gageFactor = document.getElementById("gageFactor").value;
-const bridgeResistance = document.getElementById("bridgeResistance").value;
-const strain = document.getElementById("strain").value;
-const tempCoeffRes = document.getElementById("tempCoeffRes").value;
-const deltaT = document.getElementById("deltaT").value;
+let gageFactor = document.querySelector('#gageFactor > input')
+let bridgeResistance = document.querySelector('#')
+let strain = document.querySelector('#')
+let tempCoeffRes = document.querySelector('#tempCoeffRes > value')
+let deltaT = document.querySelector('#')
+let ohmsPerInch = document.querySelector('#')
 
 //if microstrain <= 85 don't perform balance
+
+function result() {
+  let inchesOfWire = ("gageFactor" * "bridgeResistance" * "strain") / ("tempCoeffRes" * "deltaT" * "ohmsPerInch");
+  console.log("inchesOfWire");
+}
